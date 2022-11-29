37d426ab176674055d925a15f721713b7ae8edcb
Sebastian Korfmann <sebastian@korfmann.net>
2022-11-29 23:50:13 +0100

    Add another test

--- test/aws-cdk/bucket/component.test.ts
+++ test/aws-cdk/bucket/component.test.ts
@@ -1,14 +1,16 @@
-import { cdkSpec as cloud, createTestApp } from '@hekto/cloud-spec-aws-cdk'
-import { CfnOutput } from 'aws-cdk-lib'
+import { cdkSpec as cloud, createTestApp, ForceEphemeralResources } from '@hekto/cloud-spec-aws-cdk'
+import { CfnOutput, Aspects } from 'aws-cdk-lib'
 import { Bucket } from 'aws-cdk-lib/aws-s3'
 
 const testApp = createTestApp({
   creator: (stack) => {
     const component = new Bucket(stack, 'MyBucket')
 
-    new CfnOutput(stack, 'SfnArn', {
+    new CfnOutput(stack, 'BucketName', {
       value: component.bucketName,
     })
+
+    Aspects.of(stack).add(new ForceEphemeralResources())
   },
 })
 
