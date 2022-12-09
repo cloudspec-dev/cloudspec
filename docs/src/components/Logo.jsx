660aa0f5995accced1108654771cfec0b7929474
Sebastian Korfmann <sebastian@korfmann.net>
2022-12-09 14:19:02 +0100

    Add start for docs

---
+++ docs/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+export function Logomark(props) {
+  return (
+    <span className="" {...props}>CloudSpec</span>
+  )
+}
+
+export function Logo(props) {
+  return (
+    <Logomark {...props} />
+  )
+}
